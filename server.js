7e8cc24ec918633030159a45f01cae8a9350884b
Sarith21 <sarith21@gmail.com>
2013-06-08 19:57:17 +1000

    Minimum viable product
    Successfully fetches non-google-approximate addresses. Need to work out
    how to fetch best approximate address.

--- server.js
+++ server.js
@@ -1,8 +1,48 @@
 var gm = require('googlemaps');
 var http = require('http');
-var util = require('util');
+var csv = require('ya-csv');
 
-http.createServer(function (req, res) {
+var reader = csv.createCsvFileReader('sampleinput.csv');
+
+var revCoder = function(stuff) {
+
+  var coordString = "" + stuff[0] + "," + stuff[1];
+
+  gm.reverseGeocode(coordString, function(err, data){
+    
+    if (err !== null) {
+      console.log(err);
+    }
+    
+    var igra = data.results;
+    var result = "";
+    
+
+    for (var el = 0; el < igra.length; el++) {
+
+      var target = igra[el];
+
+      if (target.geometry.location_type !== "APPROXIMATE") {      
+        result = result + 
+          target.geometry.location_type + "," + 
+          target.formatted_address;
+      }
+
+    } 
+
+    console.log(stuff[0] + "," , stuff[1]+ ",", stuff[2]+ ",", result);
+
+  });
+
+};
+
+reader.addListener('data', revCoder);
+
+
+
+
+
+/*http.createServer(function (req, res) {
   gm.reverseGeocode('-33.753211,151.08205', function(err, data){
     console.log(err);
     
@@ -31,4 +71,4 @@ http.createServer(function (req, res) {
 
 }).listen(1337);
 
-console.log('Server running at http://10.0.0.10:1337/');
+console.log('Server running at http://10.0.0.10:1337/');*/
